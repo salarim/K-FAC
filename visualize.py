747963e292f10c3c4fd32d5685acfe830e4a0de0
msalari <msalari@cs-vml-23.cmpt.sfu.ca>
2021-07-05 14:49:59 -0700

    Add plot_test_acc for plotting accuracy of one model on different tasks

--- visualize.py
+++ visualize.py
@@ -111,6 +111,26 @@ def plot_fwt(filename, test_accs_dict, initial_accs):
     plt.close()
 
 
+def plot_test_acc(filename, test_accs):
+    legends = []
+    task_nb, _, _ = test_accs.shape
+    test_accs = test_accs.mean(axis=1)
+    
+    for task_id in range(task_nb):
+        if task_id == 0 or (task_id+1) % 5 == 0:
+            plt.plot(range(task_id+1,task_nb+1), test_accs[task_id:, task_id])
+            legends.append('Task {:d}'.format(task_id+1))
+
+    plt.legend(legends)
+
+    plt.ylim(0,100)
+    plt.xlabel("Training Task")
+    plt.ylabel("Test Accuracy")
+    # plt.title("Average test accuracy of a kfac-approximate continual learner")
+    plt.savefig(filename)
+    plt.close()
+
+
 def main():
     input_files = ['acc_false_e_1_tnb_50_mnb_1/acc_false_e_1_tnb_50_mnb_1_lmbd_1e2.txt',
                     'acc_false_e_1_tnb_50_mnb_1/acc_false_e_1_tnb_50_mnb_1_lmbd_1e3.txt',
@@ -137,12 +157,14 @@ def main():
         train_accs, test_accs = read_file(input_file, task_nb, model_nbs[i])
         test_accs_dict[legends[i]] = test_accs
 
-    plot_avg_test_acc('lambda_avg_acc',
-                     test_accs_dict)
+    # plot_avg_test_acc('lambda_avg_acc',
+    #                  test_accs_dict)
 
     # plot_bwt('bwt', test_accs_dict)
 
     # plot_fwt('fwt', test_accs_dict, initial_accs)
+
+    plot_test_acc('acc_per_task' + 'acc_false_e_1_tnb_50_mnb_1_lmbd_1e4', test_accs_dict['1e4'])
 
 if __name__ == '__main__':
     main()
